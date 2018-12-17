3371d57cfcc65d9b4e0a35f779e1ad8a42f26704
Anwesh43 <anweshthecool0@gmail.com>
2018-12-17 14:10:12 +0530

    adding fullscreen support

--- app/src/main/java/coml/anwesh/uiprojects/linkedpausetoplaystepview/MainActivity.kt
+++ app/src/main/java/coml/anwesh/uiprojects/linkedpausetoplaystepview/MainActivity.kt
@@ -2,6 +2,7 @@ package coml.anwesh.uiprojects.linkedpausetoplaystepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import coml.anwesh.uiprojects.pausetoplaystepview.PauseToPlayStepView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         PauseToPlayStepView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }

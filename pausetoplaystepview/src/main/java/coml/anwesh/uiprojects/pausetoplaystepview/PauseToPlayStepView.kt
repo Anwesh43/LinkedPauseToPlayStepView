20b6729913d05e651e13aa4d73d66a7c55562cee
Anwesh43 <anweshthecool0@gmail.com>
2018-12-17 10:36:22 +0530

    adding libraries and defining constants

--- pausetoplaystepview/src/main/java/coml/anwesh/uiprojects/pausetoplaystepview/PauseToPlayStepView.kt
+++ pausetoplaystepview/src/main/java/coml/anwesh/uiprojects/pausetoplaystepview/PauseToPlayStepView.kt
@@ -3,3 +3,19 @@ package coml.anwesh.uiprojects.pausetoplaystepview
 /**
  * Created by anweshmishra on 17/12/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val color : Int = Color.parseColor("#283593")
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.6f
+val lines : Int = 2

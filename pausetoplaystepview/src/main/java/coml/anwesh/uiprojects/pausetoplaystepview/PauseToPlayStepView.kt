256670d667b1635bd0bf73c8851a4f98537680d2
Anwesh43 <anweshthecool0@gmail.com>
2018-12-17 10:39:21 +0530

    adding Int and Float extension methods

--- pausetoplaystepview/src/main/java/coml/anwesh/uiprojects/pausetoplaystepview/PauseToPlayStepView.kt
+++ pausetoplaystepview/src/main/java/coml/anwesh/uiprojects/pausetoplaystepview/PauseToPlayStepView.kt
@@ -19,3 +19,13 @@ val scDiv : Double = 0.51
 val strokeFactor : Int = 90
 val sizeFactor : Float = 2.6f
 val lines : Int = 2
+
+fun Int.getInverse() : Float = 1f / this
+
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.getInverse(), Math.max(0f, this - i * n.getInverse())) * n
+
+fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
+
+fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.getInverse() + scaleFactor() * b.getInverse()
+
+fun Float.updateScale(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap 

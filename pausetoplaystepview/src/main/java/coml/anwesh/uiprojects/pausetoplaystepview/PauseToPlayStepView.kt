ee10ee17660299ee7526daf1d39051d94ea10bde
Anwesh43 <anweshthecool0@gmail.com>
2018-12-17 13:12:13 +0530

    adding PauseToPlay class

--- pausetoplaystepview/src/main/java/coml/anwesh/uiprojects/pausetoplaystepview/PauseToPlayStepView.kt
+++ pausetoplaystepview/src/main/java/coml/anwesh/uiprojects/pausetoplaystepview/PauseToPlayStepView.kt
@@ -163,7 +163,32 @@ class PauseToPlayStepView(ctx : Context) : View(ctx) {
                 return curr
             }
             cb()
-            return this 
+            return this
+        }
+    }
+
+    data class PauseToPlay(var i : Int) {
+        private val root : PTPNode = PTPNode(0)
+
+        private var curr : PTPNode = root
+
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }

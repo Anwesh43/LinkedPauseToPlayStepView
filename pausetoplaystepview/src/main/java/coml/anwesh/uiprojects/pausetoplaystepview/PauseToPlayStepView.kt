d5e812b24721551b05e827715ad52b7fe98677ad
Anwesh43 <anweshthecool0@gmail.com>
2018-12-17 10:52:08 +0530

    adding drawPTPNode method

--- pausetoplaystepview/src/main/java/coml/anwesh/uiprojects/pausetoplaystepview/PauseToPlayStepView.kt
+++ pausetoplaystepview/src/main/java/coml/anwesh/uiprojects/pausetoplaystepview/PauseToPlayStepView.kt
@@ -28,4 +28,31 @@ fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.getInverse() + scaleFactor() * b.getInverse()
 
-fun Float.updateScale(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap 
+fun Float.updateScale(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawPTPNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val xGap : Float = size * Math.sqrt(3.0).toFloat()
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.color = color
+    save()
+    translate(gap * (i + 1), h / 2)
+    rotate(-90f * sc2)
+    drawLine(-xGap/2, -size, -xGap/2, size, paint)
+    for (j in 0..(lines - 1)) {
+        val sf : Float = 1f - 2 * j
+        val sc : Float = sc1.divideScale(j, lines)
+        save()
+        translate(xGap/2, 0f)
+        rotate(30f * sc * sf)
+        drawLine(0f, 0f, 0f, size * sf, paint)
+        restore()
+    }
+    restore()
+}

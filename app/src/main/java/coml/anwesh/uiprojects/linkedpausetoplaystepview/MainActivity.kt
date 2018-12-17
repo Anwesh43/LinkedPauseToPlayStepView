81de338e453a60de68f98d4254baba6444e6f733
Anwesh43 <anweshthecool0@gmail.com>
2018-12-17 13:16:51 +0530

    adding view in activity

--- app/src/main/java/coml/anwesh/uiprojects/linkedpausetoplaystepview/MainActivity.kt
+++ app/src/main/java/coml/anwesh/uiprojects/linkedpausetoplaystepview/MainActivity.kt
@@ -2,11 +2,12 @@ package coml.anwesh.uiprojects.linkedpausetoplaystepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import coml.anwesh.uiprojects.pausetoplaystepview.PauseToPlayStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        PauseToPlayStepView.create(this)
     }
 }
